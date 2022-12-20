4e6a9daa4bdb7f752795409a4e3742717880f40b
sj-williams <steve.williams@digital.justice.gov.uk>
2022-12-20 14:25:43 +0000

    feat: refactoring constraints and config-sync manifests

--- constraints.tf
+++ constraints.tf
@@ -1,127 +1,33 @@
 # remember any kind used by a constraint template must also be added to the sync config at the end of this file
 
-resource "kubectl_manifest" "unique-ingress-template" {
-  count = var.define_constraints == true ? 1 : 0
-  depends_on = [helm_release.gatekeeper]
 
-  yaml_body = <<YAML
-apiVersion: templates.gatekeeper.sh/v1beta1
-kind: ConstraintTemplate
-metadata:
-  name: k8suniqueingresshost
-  annotations:
-    description: Requires all Ingress hosts to be unique, unless they share a namespace.
-spec:
-  crd:
-    spec:
-      names:
-        kind: k8suniqueingresshost
-  targets:
-    - target: admission.k8s.gatekeeper.sh
-      rego: |
-        package k8suniqueingresshost
-        identical(obj, review) {
-          obj.metadata.namespace == review.object.metadata.namespace
-          obj.metadata.name == review.object.metadata.name
-        }
-        same_namespace(obj, review) {
-          obj.metadata.namespace == review.object.metadata.namespace
-        }
-        violation[{"msg": msg}] {
-          input.review.kind.kind == "Ingress"
-          re_match("^(extensions|networking.k8s.io)$", input.review.kind.group)
-          host := input.review.object.spec.rules[_].host
-          other := data.inventory.namespace[ns][otherapiversion]["Ingress"][name]
-          re_match("^(extensions|networking.k8s.io)/.+$", otherapiversion)
-          other.spec.rules[_].host == host
-          namespace := other.metadata.namespace
-          not identical(other, input.review)
-          not same_namespace(other, input.review)
-          msg := sprintf("ingress host conflicts with an existing ingress <%v> in namespace <%v>", [host, namespace])
-        }
-YAML
+data "kubectl_filename_list" "constraint-templates" {
+    pattern = "${path.module}/resources/constraints/*_template.yaml"
 }
 
-resource "kubectl_manifest" "unique-ingress-constraint" {
-  count = var.define_constraints == true ? 1 : 0
-  depends_on = [kubectl_manifest.unique-ingress-template]
-
-  yaml_body = <<YAML
-apiVersion: constraints.gatekeeper.sh/v1beta1
-kind: k8suniqueingresshost
-metadata:
-  name: k8suniqueingresshost
-spec:
-  match:
-    kinds:
-      - apiGroups: ["extensions", "networking.k8s.io"]
-        kinds: ["Ingress"]
-YAML
+data "kubectl_filename_list" "constraints" {
+  pattern = "${path.module}/resources/constraints/*_constraint.yaml"
 }
 
-resource "kubectl_manifest" "ingress-default-modsec-template" {
-  count = var.define_constraints == true ? 1 : 0
-  depends_on = [helm_release.gatekeeper]
-
-  yaml_body = <<YAML
-apiVersion: templates.gatekeeper.sh/v1beta1
-kind: ConstraintTemplate
-metadata:
-  name: k8sdenydefaultmodsec
-  annotations:
-    description: This policy denies ingresses using default ingress-controller if they try to enable modsecurity.
-spec:
-  crd:
-    spec:
-      names:
-        kind: k8sdenydefaultmodsec
-  targets:
-    - target: admission.k8s.gatekeeper.sh
-      rego: |
-        package k8sdenydefaultmodsec
-        violation[{"msg": msg}] {
-          input.review.kind.kind == "Ingress"
-          input.review.object.metadata.annotations["kubernetes.io/ingress.class"] == "nginx"
-          input.review.object.metadata.annotations["nginx.ingress.kubernetes.io/enable-modsecurity"] == "true"
-          msg := "mod-security is not allowed for default ingress"
-        }
-        violation[{"msg": msg}] {
-          input.review.kind.kind == "Ingress"
-          not input.review.object.metadata.annotations["kubernetes.io/ingress.class"]
-          input.review.object.metadata.annotations["nginx.ingress.kubernetes.io/enable-modsecurity"] == "true"
-          msg := "mod-security is not allowed for default ingress"
-        }
-        violation[{"msg": msg}] {
-          input.review.kind.kind == "Ingress"
-          input.review.object.metadata.annotations["kubernetes.io/ingress.class"] == "nginx"
-          input.review.object.metadata.annotations["nginx.ingress.kubernetes.io/modsecurity-snippet"]
-          msg := "modsecurity-snippet is not allowed for default ingress"
-        }
-        violation[{"msg": msg}] {
-          input.review.kind.kind == "Ingress"
-          not input.review.object.metadata.annotations["kubernetes.io/ingress.class"]
-          input.review.object.metadata.annotations["nginx.ingress.kubernetes.io/modsecurity-snippet"]
-          msg := "modsecurity-snippet is not allowed for default ingress"
-        }
-YAML
+resource "kubectl_manifest" "constraint-templates" {
+  depends_on    = [helm_release.gatekeeper]
+  count         = length(data.kubectl_filename_list.constraint-templates.matches)
+  yaml_body     = file(element(data.kubectl_filename_list.constraint-templates.matches, count.index))
 }
 
-resource "kubectl_manifest" "ingress-default-modsec-constraint" {
-  count = var.define_constraints == true ? 1 : 0
-  depends_on = [kubectl_manifest.ingress-default-modsec-template]
-
-  yaml_body = <<YAML
-apiVersion: constraints.gatekeeper.sh/v1beta1
-kind: k8sdenydefaultmodsec
-metadata:
-  name: k8sdenydefaultmodsec
-spec:
-  match:
-    kinds:
-      - apiGroups: ["extensions", "networking.k8s.io"]
-        kinds: ["Ingress"]
-YAML
+resource "kubectl_manifest" "constraints" {
+  depends_on    = [helm_release.gatekeeper]
+  count         = length(data.kubectl_filename_list.constraints.matches)
+  yaml_body     = file(element(data.kubectl_filename_list.constraints.matches, count.index))
 }
+
+# Resources to be synced here
+resource "kubectl_manifest" "config-sync" {
+  depends_on    = [helm_release.gatekeeper]
+
+  yaml_body     = file("${path.module}/resources/config_sync.yaml")
+}
+
 
 /* This dosn't work, to be fixed in next PR
 resource "kubectl_manifest" "pod-tolerations-template" {
@@ -172,32 +78,3 @@ YAML
 */
 
 /* add resources to sync here */
-resource "kubectl_manifest" "config-sync" {
-  depends_on = [helm_release.gatekeeper]
-
-  yaml_body = <<YAML
-apiVersion: config.gatekeeper.sh/v1alpha1
-kind: Config
-metadata:
-  name: config
-  namespace: "gatekeeper"
-spec:
-  sync:
-    syncOnly:
-      - group: "extensions"
-        version: "v1"
-        kind: "Ingress"
-      - group: "networking.k8s.io"
-        version: "v1"
-        kind: "Ingress"
-      - group: ""
-        version: "v1"
-        kind: "Namespace"
-      - group: ""
-        version: "v1"
-        kind: "Pod"
-      - group: ""
-        version: "v1"
-        kind: "Service"
-YAML
-}
