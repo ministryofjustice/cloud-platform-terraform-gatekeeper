8c2e2d7b72e29167d0aeb0de8aa45467af3a559c
sj-williams <steve.williams@digital.justice.gov.uk>
2022-12-20 17:33:12 +0000

    test: update gh unit test workflow and tf unit tests

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -1,35 +1,101 @@
-provider "aws" {
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-  region                      = "eu-west-2"
-  s3_force_path_style         = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
+terraform {
+  required_version = ">= 0.14"
+}
 
-  endpoints {
-    apigateway     = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    elasticache    = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    rds            = "http://localhost:4566"
-    redshift       = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    s3             = "http://localhost:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "16.14.1"
+  namespace  = "monitoring"
+  depends_on = [kubernetes_namespace.monitoring]
+
+  set {
+    name  = "defaultRules.create"
+    value = "false"
+  }
+  set {
+    name  = "alertmanager.enabled"
+    value = "false"
+  }
+  set {
+    name  = "grafana.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubeApiServer.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubelet.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubeControllerManager.enabled"
+    value = "false"
+  }
+  set {
+    name  = "coreDns.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubeDns.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubeEtcd.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubeScheduler.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubeProxy.enabled"
+    value = "false"
+  }
+  set {
+    name  = "kubeStateMetrics.enabled"
+    value = "false"
+  }
+  set {
+    name  = "nodeExporter.enabled"
+    value = "false"
+  }
+  set {
+    name  = "prometheusOperator.enabled"
+    value = "false"
+  }
+  set {
+    name  = "prometheus.enabled"
+    value = "true"
+  }
+
+}
+
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.4.0"
+  namespace  = "cert-manager"
+  depends_on = [kubernetes_namespace.cert_manager, helm_release.prometheus]
+
+  set {
+    name  = "installCRDs"
+    value = "true"
   }
 }
 
