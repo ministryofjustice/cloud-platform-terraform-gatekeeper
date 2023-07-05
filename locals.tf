9453d199e5a6fbb103f5e615e214526d471d7caf
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-07-05 15:56:04 +0100

    feat: 🎸 add ingress hostname length constraint (#19)

--- locals.tf
+++ locals.tf
@@ -5,5 +5,6 @@ locals {
     modsec_snippet_nginx_class = merge(yamldecode(file("${path.module}/resources/constraints/modsec_snippet_nginx_class.yaml")), { "spec" : { "enforcementAction" : var.dryrun_map.modsec_snippet_nginx_class ? "dryrun" : "deny" } })
     modsec_nginx_class         = merge(yamldecode(file("${path.module}/resources/constraints/modsec_nginx_class.yaml")), { "spec" : { "enforcementAction" : var.dryrun_map.modsec_nginx_class ? "dryrun" : "deny" } })
     ingress_clash              = merge(yamldecode(file("${path.module}/resources/constraints/ingress_clash.yaml")), { "spec" : { "enforcementAction" : var.dryrun_map.ingress_clash ? "dryrun" : "deny" } })
+    hostname_length            = merge(yamldecode(file("${path.module}/resources/constraints/ingress_hostname_length.yaml")), { "spec" : { "enforcementAction" : var.dryrun_map.hostname_length ? "dryrun" : "deny" } })
   }
 }
